7e890e9b9ecc11d60b7d117bc86ea84bcbf41423
megalon <megalongaming@gmail.com>
2025-07-06 08:21:32 -0400

    Init all files

---
+++ src/PeakKrustyKrabPizza/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace PeakKrustyKrabPizza
+{
+    [BepInAutoPlugin]
+    public partial class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log { get; private set; } = null!;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            Log.LogInfo($"Plugin {Name} is loaded!");
+        }
+    }
+}
